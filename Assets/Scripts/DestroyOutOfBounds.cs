a4140645ec63c16cf8a84b70e0268ab9472668f5
joshua king <jking24@aoiths.org>
2023-02-01 11:56:14 -0500

    small changes

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30;
-    private float lowerBound = -10;
-    private float Leftbound
+	private float topBound = 30;
+	private float lowerBound = -10;
+	//** Create a float variable below that is set to the X value of when animals leave the screen and name it sideBounds**//
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	GameManager GameManagerScript;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.z > topBound)
-        {
-            Destroy(gameObject);
-        }
-        else if (transform.position.z < lowerBound)
-        {
-            Debug.Log("Game over!");
-            Destroy(gameObject);
-        }
+	// Start is called before the first frame update
+	void Start()
+	{
+		GameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.z > topBound)
+		{
+			Destroy(gameObject);
+		}
+		else if (transform.position.z < lowerBound)
+		{
+			//** Pass an integer in the method below that reduces your player's life **//
+			//GameManagerScript.UpdateLives(PassIntegerHere);
+			Destroy(gameObject);
+		}
+
+		//** Uncomment the if statement below once the sideBound variables are declared **//
+		// if (transform.position.x < -sideBounds || transform.position.x > sideBounds )
+		// {
+		// 	GameManagerScript.UpdateLives(-1);
+		// 	Destroy(gameObject);
+		// }
+	}
 }
 
-}
